b028cb40d93edef178e6cc7f1d1b66db513714c6
Jane Smith <aeglamorous@gmail.com>
2018-04-26 11:55:37 +0700

    start with styled-components

---
+++ components/layouts/header.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Nav = styled.ul`
+  list-style: none;
+  padding-left: 0px;
+`;
+const NavList = styled.li`
+  display: inline-block;
+`;
+const NavLink = styled.a`
+  display: inline-block;
+  padding: 8px 15px;
+`;
+
+const NavItem = props => (
+  <NavList>
+    <Link href={props.href}>
+      <NavLink>{props.title}</NavLink>
+    </Link>
+  </NavList> 
+);
+
+const Header = () => (
+  <Nav className="list list-inline">
+    <NavItem title="HOME" href="/" />
+    <NavItem title="ABOUT" href="/about" />
+    <NavItem title="CONTACT" href="/contact" />
+  </Nav>
+)
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layouts/index';
+
+const Index = () => (
+  <Layout>
+    <h1>Sawatdee Next.js</h1>
+  </Layout>
+)
+
+export default Index;
